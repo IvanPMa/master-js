42dfab97664d3586191f4ef8b9863a6d89a89c89
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-19 13:56:43 -0600

    Alerta,confirmacion e ingreso de datos

---
+++ js/10-ventanas.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// ALERT
+
+// alert("ESta es mi alerta");
+// alert ("Este es mi texto"); 
+
+//Confirmacion
+
+ var resultado = confirm("¿Estas seguro de querer continuar?");
+ console.log(resultado);
+
+ //Ingreso de datos
+
+ // prompt ("Texto a mostrar en la ventana", valor por defecto)
+ var mi_resultado = prompt ("¿Que edad tienes?", 18);
+ console.log(typeof mi_resultado);
