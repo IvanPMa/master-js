4d8ce5d17588fc657fabbebd4a1aac030d7fb195
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-19 14:22:38 -0600

    Ejercicio 2

--- js/12-ejercicio-2.js
+++ js/12-ejercicio-2.js
@@ -1,6 +1,13 @@
 'use strict'
 
 /* 
-    Utilizando un blucle, msotrar la suma y la media de los numeros introducidos hasta introducir un numero negativo y ahi 
+    Utilizando un blucle, mostrar la suma y la media de los numeros introducidos hasta introducir un numero negativo y ahi 
     mostrar el resultado
 */
+var suma = 0;
+do{
+    var numero = parseInt(prompt("Ingresa un numero: ",0));
+    numero>= 0? suma+= numero : console.log ("Numero negativo, fin del bucle");
+}while (numero >= 0);
+
+console.log("Suma: ",suma);
