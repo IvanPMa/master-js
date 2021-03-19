844fc0af125ab86fb7a43167f5f53a75fe14a842
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-18 22:42:52 -0600

    Operadores y tipos de datos

---
+++ js/05-tipos-de-datos.js
@@ -0,0 +1,23 @@
+'use strict'
+// Operadores
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero2 * numero2;
+
+// alert("El resultado de la operacion es: "+operacion);
+
+//Tipos de datos
+
+var numero_entero = 44;
+var cadena_texto = "Hola 'que' tal";
+console.log(cadena_texto);
+var verdadero_o_falso = true;
+console.log(verdadero_o_falso);
+
+var numero_falso = "33.4";
+console.log(parseFloat(numero_falso)+7);
+
+
+var numero_letra = 23;
+
+console.log(String(numero_letra));
