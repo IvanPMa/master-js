17728839e9cba2c91568c38a5a1f40c477a9b9c8
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-19 16:58:15 -0600

    Ejercicio 6

--- js/16-ejercicio-6.js
+++ js/16-ejercicio-6.js
@@ -1,7 +1,11 @@
 'use strict'
-
 /*
      Que nos diga si el numero es par o inpar
      1.Ventana de prompt
      2. Si no es valido que nos pida de nuevo el numero
 */
+do {
+    var numero = parseInt(prompt("Numero ",5));
+} while (isNaN(numero) || numero<= 0);
+
+numero % 2 == 0 ? alert("El numero es par") :alert("El numero es inpar");
