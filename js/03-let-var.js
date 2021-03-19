7ffe8cbac444ab903b4cb97b319d4b17f5c18d58
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-18 22:48:19 -0600

    Let y var

---
+++ js/03-let-var.js
@@ -0,0 +1,23 @@
+'use strict'
+//Prueba con var
+var numero = 40;
+console.log(numero); //valor 40
+
+if (true){
+    var numero = 50; 
+    console.log(numero); //valor 50
+}
+
+console.log(numero);
+
+//Prueba con let
+
+var texto = "Curso JS";
+console.log(texto); // Valor Js
+
+if (true){
+    let texto = "Curso TypeScript";
+    console.log(texto); //Curso de TS
+}
+
+console.log(texto); // valor js
