8504be57eecce3f3c3e78f13f43a087e380fb486
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-18 22:32:54 -0600

    Variables

---
+++ js/02-variables.js
@@ -0,0 +1,11 @@
+// VARIABLES
+var pais = "España";
+var continente = "Europa"
+var antiguedad = 2019;
+var pais_y_continente = pais + " "+ continente;
+
+pais = "México";
+continente = "America";
+
+console.log(pais,continente,antiguedad);
+alert(pais_y_continente);
