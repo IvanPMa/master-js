c9d87dafd8a22393b7e72412a96e5def2a1c33bd
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-21 14:48:35 -0600

    Funciones

---
+++ js/19-funciones.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//Funciones
+//una agrupacion reutilizable de un conjunto de isntrucciones
+
+function calculadora() {
+    console.log("Hola soy la calculadora");
+    console.log("Si soy yo ");
+    return "Hola soy la calculadora"
+}
+
+var mensajeCalculadora = calculadora();
+console.log(mensajeCalculadora);
