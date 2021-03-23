984713fb0f916b64afddaee7c994f6e5d831269c
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-23 16:23:34 -0600

    Arrays - Length

--- js/25-arrays.js
+++ js/25-arrays.js
@@ -5,6 +5,20 @@
 var nombre= "Ivan Pacheco";
 var nombres = ["Ivan Pacheco","Victor Robles","Juan Lopez","Manolo Garcia",52,true]
 
-var lenguajes = new Array('HTMl', 'JS', 'TS', 'Java');
+var lenguajes = new Array('HTMl', 'JS', 'TS', 'Java','C#','Python');
 
-console.log(nombres[0]);
+/*
+var elemento = parseInt(prompt("¿Que elemento del array quieres? ",0));
+if(elemento >= nombres.length){
+    alert("Introduce el numero correcto menor que " + nombres.length)
+}else{
+    alert("El usuario seleccionado es " +nombres[elemento]);
+}
+*/
+
+document.write("<h1>Lenguajes del 2021</h1>");
+document.write("<ul>");
+for (let i = 0 ; i < lenguajes.length; i++){
+    document.write("<li>"+ lenguajes[i]+"</li>");
+}
+document.write("</ul>");
