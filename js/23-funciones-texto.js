140dbea791f795b99d4a4b983a68e4c9614b6046
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-23 14:21:27 -0600

    Metodos de busqueda

--- js/23-funciones-texto.js
+++ js/23-funciones-texto.js
@@ -3,23 +3,36 @@
 // Transformacion de textos
 
 var numero = 444;
-var texto1 = " Curso de JS";
+var texto1 = "Este es un curso de Javascript curso";
 var texto2 ="Es muy buen curso";
+// Index Of indica en que caracter inicia esa palabra
+let busqueda1 = texto1.lastIndexOf("curso");
+console.log(busqueda1);
 
-var dato = numero.toString();
-    dato = texto1.toUpperCase();
-    dato = texto2.toLowerCase();
-// console.log (dato);
+// search al igual que inexOf nos dice en que posicon comienza la palaabra
+let busqueda2 = texto1.search("curso");
+console.log (busqueda2);
 
-//Calcular longitud de textos
-var nombre = "Ivan Pacheco";
-    nombre = ["hola", "hola"]
+//match devuelve un array con las palabras 
+//Con solo la palabra entre comillas devuelve un array solo con el primer resultado
+// Si queremos qeu encuentre todas las coincidencias con la palabra debemos hacer
+//uso de las expresiones regulares
+let busqueda3 = texto1.match(/curso/g);
+console.log(busqueda3);
 
-// console.log(nombre.length);
+//Con sbstr extraemos una cadena de caracteres donde indicamos donde inicia y cuantos caracteres
+// extraemos
+let busqueda4 = texto1.substr(11,5);
+console.log(busqueda4);
 
-//Concatenar - unir textos
+//Obtenemos una letra concreta de un string, indicando el indice
+let busqueda5 = texto1.charAt(5);
+console.log(busqueda5);
 
-//var textoTotal = texto1 +" "+  texto2;
+//Busca al inicio del string que empiece con esa cadena
+let busqueda6 = texto1.startsWith("Este es un curso");
+console.log (busqueda6);
+console.log(texto1.endsWith("Javascript curso"))
 
-var textoTotal = texto1.concat(" "+texto2);
-console.log (textoTotal);
+//Encontrar la palabra dentro del string con includes
+console.log(texto1.includes("Javascript"));
