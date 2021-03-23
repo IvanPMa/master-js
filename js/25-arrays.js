3cf5c978d943a64cd24ad6aea258d1b8715bf95a
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-23 15:08:36 -0600

    Arrays

---
+++ js/25-arrays.js
@@ -0,0 +1,10 @@
+'use strict'
+
+//Arrays , Arreglos, Matrices
+
+var nombre= "Ivan Pacheco";
+var nombres = ["Ivan Pacheco","Victor Robles","Juan Lopez","Manolo Garcia",52,true]
+
+var lenguajes = new Array('HTMl', 'JS', 'TS', 'Java');
+
+console.log(nombres[0]);
