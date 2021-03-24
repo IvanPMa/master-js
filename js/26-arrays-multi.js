af79d26161b37a3180cab4f11b7a34368e11e53e
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-03-23 19:45:45 -0600

    Arrays multidimensionales

---
+++ js/26-arrays-multi.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var categorias = ['Accion','Terror','Comedia'];
+var peliculas = ['El infierno','Amarte duele','Amores perros']
+
+var cine = [categorias,peliculas]
+
+console.log (cine[0][1]);
+console.log(cine[1][2])
+
