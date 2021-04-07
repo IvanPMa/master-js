e707ea8c8bb4aea4e638b639533fb5f5a3173099
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-06 20:05:37 -0500

    clase DOM

--- js/28-dom.js
+++ js/28-dom.js
@@ -1,5 +1,8 @@
 'use strict'
 // DOM - Document Object model
+function cambiaColor (color){
+    caja.style.background = color;
+} 
 
 var caja = document.getElementById("micaja");
 caja.innerHTML = "¡Texto desde JS¡";
@@ -8,4 +11,7 @@ caja.innerHTML = "¡Texto desde JS¡";
 caja.style.background = "red";
 caja.style.padding = "20px";
 caja.style.color = "white";
+//Añadir una clase
+caja.className = "caja";
+// cambiaColor("blue");
 console.log(caja);
