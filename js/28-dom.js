9083d7dbc48f1bbb1a4de504a4185d5f4427f21c
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-06 19:56:00 -0500

    estilos dom

---
+++ js/28-dom.js
@@ -0,0 +1,9 @@
+'use strict'
+// DOM - Document Object model
+
+var caja = document.getElementById("micaja");
+caja.innerHTML = "¡Texto desde JS¡";
+
+//Cambiar estilos desde js
+caja.style.background = "red";
+console.log(caja);
