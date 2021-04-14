2bebd8d0103e86751afe8a1bd2b87b26ac750168
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-14 18:02:49 -0500

    BOM

--- js/29-bom.js
+++ js/29-bom.js
@@ -3,7 +3,19 @@
 function getBom(){
     console.log(screen.width);
     console.log(screen.height);
-    console.log(window.location);
+    console.log(window.location.href);
 }
 
 getBom();
+
+// funcion para redirigirnos a otra pagina
+
+function redirect(url){
+    window.location.href = url;
+}                                                      
+
+// funcion para abrir una pagina en una nueva pestaña
+
+function abrirVentana(url){
+    window.open(url, "","width=400,height=300");
+}
