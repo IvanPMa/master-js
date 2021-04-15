2a6f041579417b7738f00429187b78b3143aad6d
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-14 22:54:06 -0500

    Event listener

--- js/30-eventos.js
+++ js/30-eventos.js
@@ -1,15 +1,17 @@
 'use strict'
 
 //Eventos del ratón
-
-var boton = document.querySelector("#boton");
-
-function cambiarColor(){
-    
+function cambiarColor(){    
     var bg = boton.style.background;
     if( bg == 'green'){
         boton.style.background = "red";
-    } else{
+    } else{ 
         boton.style.background = "green";
     }
+    boton.style.padding = '10px';
+    boton.style.border = "2px solid #ccc";
 }
+var boton = document.querySelector("#boton");
+boton.addEventListener('click', ()=>{
+    cambiarColor();
+});
