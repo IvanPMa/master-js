f4cec0720a67a45b48d67a3ea63c6945440302a3
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-14 22:34:59 -0500

    cambiar de color clasico

--- js/30-eventos.js
+++ js/30-eventos.js
@@ -1,3 +1,15 @@
 'use strict'
 
 //Eventos del ratón
+
+var boton = document.querySelector("#boton");
+
+function cambiarColor(){
+    
+    var bg = boton.style.background;
+    if( bg == 'green'){
+        boton.style.background = "red";
+    } else{
+        boton.style.background = "green";
+    }
+}
