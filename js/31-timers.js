dd927dbe7014099440d5a7fae4a11a152e45084b
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-20 17:32:15 -0500

    set interval, stop, start

--- js/31-timers.js
+++ js/31-timers.js
@@ -1,21 +1,33 @@
 'use strict'
 window.addEventListener('load', function(){
-    // Timers
-var tiempo = setInterval( function (){
-    console.log("Set interval ejecutado");
-
-    var encabezado = document.querySelector('h1');
-    if( encabezado.style.fontSize == '50px'){
-        encabezado.style.fontSize = '30px'
-    } else{
-        encabezado.style.fontSize = '50px'
+    function intervalo(){
+        var tiempo = setInterval( function (){
+            console.log("Set interval ejecutado");
+    
+            var encabezado = document.querySelector('h1');
+            if( encabezado.style.fontSize == '50px'){
+                encabezado.style.fontSize = '30px'
+            } else{
+                encabezado.style.fontSize = '50px'
+            }
+        }, 1000);
+        return tiempo;    
     }
-    }, 1000);
+    
+    // Timers
+    
+    var tiempo = intervalo();
 
     var stop = document.querySelector('#stop');
     stop.addEventListener('click', function(){
         alert('Haz parado el intervalo en bucle');
         clearInterval(tiempo);
+    });
+
+    var start = document.querySelector('#start');
+    start.addEventListener('click', function(){
+        alert('Haz iniciado el intervalo en bucle');
+        intervalo();
     });
 })
 
