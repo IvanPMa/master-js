a8bbe96bda338063b5213a419c5afbc9fab592c5
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-21 19:25:30 -0500

    onSubmit

--- hola mundo/js/32-ejercicio-dom.js
+++ hola mundo/js/32-ejercicio-dom.js
@@ -1,4 +1,11 @@
 'use strict'
 window.addEventListener('load',function(){
     console.log('DOM Cargado');
+    
+    var formulario = document.querySelector('#formulario');
+
+    formulario.addEventListener('submit', function(){
+        console.log('Evento sumbit capturado');
+    })
+
 });
