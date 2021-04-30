ff6591716e3bd45dc0493cd0d4f924fc4c8b195b
IvanPMa <pachecomaldonadoivan@gmail.com>
2021-04-30 17:39:57 -0500

    box dashed display

--- hola mundo/js/32-ejercicio-dom.js
+++ hola mundo/js/32-ejercicio-dom.js
@@ -4,11 +4,17 @@ window.addEventListener('load',function(){
     
     var formulario = document.querySelector('#formulario');
 
+    var box_dashed = document.querySelector('.dashed');
+    box_dashed.style.display = 'none';
+
     formulario.addEventListener('submit', function(){
         console.log('Evento sumbit capturado');
         var nombre = document.querySelector('#nombre').value;
         var apellidos = document.querySelector('#apellidos').value;
         var edad = document.querySelector('#edad').value;
+
+        box_dashed.style.display = 'block';
+        
         console.log(nombre,apellidos,edad);
     })
 
